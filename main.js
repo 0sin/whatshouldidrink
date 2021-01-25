3a2aa35a71e87899087aa5e493c7a37298db1842
0sin <69961808+0sin@users.noreply.github.com>
2021-01-26 00:24:27 +0900

    Test mobile size

--- main.js
+++ main.js
@@ -55,6 +55,16 @@ function hideQHeader() {
 
 
 
+// Waiting Page
+function waitingAnimation() {
+  const waitingBar = document.querySelector('.bar_width');
+  waitingBar.style = 'animation: waitingBar 3s ease-in-out;'
+  // waitingBar.style.animation = 'waitingBar 2s ease-in-out;'
+  console.log('프로그레스바실행')
+}
+
+
+
 
 
 // Question Page 인덱스 별 Event
@@ -72,8 +82,8 @@ function findIdx() {
   if (qIdx === qItem.length - 1) {
     // q_slide 끄고 결과창 보여주기
     hideQHeader();
-    lastPage3secDisplay = setTimeout(hideQSlidePage, 5000);
-    
+    waitingAnimation();
+    lastPage3secDisplay = setTimeout(hideQSlidePage, 3500);
   }
 }
 
@@ -129,7 +139,7 @@ function choiceBeverage() {
     if (resultObj.milk === 'yes' || resultObj.milk === 'nomatter') {
       if (resultObj.sugar === 'yes' || resultObj.sugar === 'nomatter') {
         if (resultObj.citrus === 'yes' || resultObj.citrus === 'nomatter') {
-          return resultBeverage = '블론드 바닐라 더블샷';
+          return resultBeverage = '블론드 바닐라 더블샷 마끼야또';
         } 
         else {
           return resultBeverage = '돌체 라떼';
@@ -210,6 +220,120 @@ function showName() {
   resultUserName.innerHTML = nameText;
 }
 
+function showResultImgAndDesc() {
+  const resultImg = document.querySelector('.result_img')
+  const resultDesc = document.querySelector('.result_desc')
+  let imgSrc = '';
+  let imgAlt = '';
+  let description = '';
+
+  switch (resultBeverage) {
+    case '블론드 바닐라 더블샷 마끼야또':
+      imgSrc = './img/starbucks/blond_ice.jpg';
+      imgAlt = '블론드 바닐라 더블샷 마끼야또';
+      description = '블론드 에스프레소 2샷에 흑당 시럽과 바닐라 크림이 부드럽고 달콤하게 어우러진 마키아또 타입의 음료를 즐겨 보세요!'
+      break;
+
+    case '돌체 라떼':
+      imgSrc = './img/starbucks/dolce_ice.jpg';
+      imgAlt = '돌체 라떼'
+      description = '깊은 에스프레소와 깔끔한 무지방 우유가 어우러진 달콤한 음료.';
+      break;
+      
+    case '스타벅스 더블샷':
+      imgSrc = './img/starbucks/double_shot.jpg';
+      imgAlt = '스타벅스 더블샷'
+      description = '신선하게 제조된 더블 샷 믹스에 클래식 시럽을 넣고 에스프레소 샷, 얼음이 어우러져 핸드 쉐이킹한 음료입니다.';
+      break;
+
+    case '바닐라 크림 콜드 브루':
+      imgSrc = './img/starbucks/vanilla_coldbrew.jpg';
+      imgAlt = '바닐라 크림 콜드 브루'
+      description = '콜드 브루에 더해진 바닐라 크림으로 깔끔하면서 달콤한 콜드 브루를 새롭게 즐길 수 있는 음료입니다.';
+      break;
+
+    case '콜드 브루 플로트':
+      imgSrc = './img/starbucks/float.jpg';
+      imgAlt = '콜드 브루 플로트'
+      description = '[리저브 전용음료] 리저브 콜드 브루 위에 녹아 내리는 한 스쿱의 바닐라 아이스크림';
+      break;
+
+    case '더블 에스프레소칩 프라프치노':
+      imgSrc = './img/starbucks/double_espresso.jpg';
+      imgAlt = '더블 에스프레소칩 프라프치노'
+      description = '';
+      break;
+
+    case '콜드 브루':
+      imgSrc = './img/starbucks/cold_brew.jpg';
+      imgAlt = '콜드 브루'
+      description = '콜드 브루 전용 원두를 차가운 물로 14시간 동안 추출하여 한정된 양만 제공됩니다. 깊은 풍미의 새로운 커피 경험을 즐겨보세요.';
+      break;
+
+    case '오늘의 커피':
+      imgSrc = './img/starbucks/todays_coffee_ice.jpg';
+      imgAlt = '오늘의 커피'
+      description = '신선하게 브루드(Brewed)되어 원두의 다양함이 살아있는 커피';
+      break;
+
+    case '화이트 딸기 크림 프라푸치노':
+      imgSrc = './img/starbucks/white_strawberry_cream.jpg';
+      imgAlt = '화이트 딸기 크림 프라푸치노'
+      description = '달콤한 딸기 음료의 새로운 버전. 딸기에 눈이 소복이 쌓인 것 같은 비주얼에 풍부한 딸기 맛을 가볍게 즐길 수 있는 누구나 좋아하는 딸기 음료. 바닐라 크림 베이스로 더욱 깊은 딸기의 맛과 풍미를 즐겨보세요';
+      break;
+
+    case '허니 얼 그레이 밀크티':
+      imgSrc = './img/starbucks/honey_earl_grey_hot.jpg';
+      imgAlt = '허니 얼 그레이 밀크티'
+      description = '베로가못 향 가득한 진한 티바나 얼 그레이 티에 꿀이 더해져 달콤하고 부드러운 밀크 티';
+      break;
+
+    case '딸기 요거트':
+      imgSrc = './img/starbucks/yogurt.jpg';
+      imgAlt = '딸기 요거트'
+      description = '딸기와 요거트의 상큼함이 가득 느껴지는 가벼운 컨셉의 블렌디드 음료입니다.';
+      break;
+
+    case '제주 유기농 말차로 만든 라떼':
+      imgSrc = './img/starbucks/jeju_ice.jpg';
+      imgAlt = '제주 유기농 말차로 만든 라떼'
+      description = '차광재배한 어린 녹찻잎을 곱게 간 제주 유기농 말차를 사용하였습니다. 깊고 진한 말차 본연의 맛과 향을 시원하고 부드럽게 즐길 수 있는 제주 유기농 말차로 만든 라떼를 만나보세요.';
+      break;
+
+    case '자몽 허니 블랙티':
+      imgSrc = './img/starbucks/honey_black_ice.jpg';
+      imgAlt = '자몽 허니 블랙티'
+      description = '새콤달콤한 자몽과 꿀이 깊고 그윽한 풍미의 스타벅스 티바나의 블랙 티와 만났습니다. 화려한 색감과 톡 쏘는 상큼함이 더해진 스타벅스의 새로운 티 음료입니다.';
+      break;
+
+    case '자바 칩 프라푸치노':
+      imgSrc = './img/starbucks/javachip.jpg';
+      imgAlt = '자바 칩 프라푸치노'
+      description = '커피 프라푸치노에 초콜릿, 초콜릿 칩이 첨가된 아이스 블렌드로 달콤 아삭한 음료입니다.';
+      break;
+
+    case '망고 바나나 블렌디드':
+      imgSrc = './img/starbucks/mango.jpg';
+      imgAlt = '망고 바나나 블렌디드'
+      description = '[Grande Only] 인기 음료인 망고 패션후르츠 블렌디드에 신선한 바나나 1개가 통째로 들어간 달콤한 프라푸치노';
+      break;
+
+    case '제주 유기 녹차':
+      imgSrc = './img/starbucks/green_tea_hot.jpg';
+      imgAlt = '제주 유기 녹차'
+      description = '우수한 품질의 제주도 산 유기 녹차로만 이루어져 맑은 수색과 신선한 향, 맛이 뛰어난 녹차 입니다.';
+      break;
+
+
+    default:
+      console.log('no-img');
+  }
+
+  resultImg.setAttribute('src', imgSrc);
+  resultImg.setAttribute('alt', imgAlt);
+  resultDesc.innerText = description;
+}
+
 
 
 
@@ -223,12 +347,15 @@ function showResultPage() {
   const resultPage = document.querySelector('.result_page');
   resultPage.style.display = 'block';
 
+
+  console.log('결과 실행')
   // 결과값 보여주기 
   choiceBeverage();
   showResultBeverage();
   showName();
+  showResultImgAndDesc();
   
-  console.log('결과 실행')
+  
 }
 
 function hideQSlidePage() {
